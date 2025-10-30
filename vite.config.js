c1faee50209862211c0e2b9e9c60fa4d4cd72a6d
Ayemen <abdulkareemayemen@Ayemens-MacBook-Air.local>
2025-10-30 08:03:47 +0530

    package updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WeatherReport/',  
+  base: prcoess.env.VITE_BASE_PATH || '/WeatherReport',  
 });
